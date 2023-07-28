2cd0604e676c434faeee5c9b54bc447d9135b72e
thisisjab <heythisisjab@gmail.com>
2023-07-28 16:04:18 +0330

    chore: set  in project settings to user

--- project/settings.py
+++ project/settings.py
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication configuration
+AUTH_USER_MODEL = 'core.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
