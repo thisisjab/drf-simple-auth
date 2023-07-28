0cc2550eab1d5f886b15c41500c27fefb82e8269
thisisjab <heythisisjab@gmail.com>
2023-07-28 17:15:22 +0330

    chore: add core.backends.AuthenticationBackend to AUTHENTICATION_BACKENDS in project settings

--- project/settings.py
+++ project/settings.py
@@ -112,7 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Authentication configuration
 AUTH_USER_MODEL = 'core.User'
-
+AUTHENTICATION_BACKENDS = [
+    'core.backends.AuthenticationBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
