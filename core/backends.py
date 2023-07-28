d846a9494c459fc39a5b9ef966842e7bf83de19f
thisisjab <heythisisjab@gmail.com>
2023-07-28 17:11:21 +0330

    feat: create authentication backend in `core` app
    This authentication backend allows users to use their email or username with their password to login.
    User needs to have its email verified in advance unless login will raise a validation error.

---
+++ core/backends.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import backends, get_user_model
+from django.db.models import Q
+from django.core.exceptions import ValidationError
+
+
+class AuthenticationBackend(backends.ModelBackend):
+    """
+    Custom authentication backend for logging in using email or username
+    with password
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return
+        try:
+            UserModel = get_user_model()
+            user = UserModel.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username)
+            )
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+            UserModel().set_password(password)
+        else:
+            if not user.is_email_activated:
+                raise ValidationError('Your email is not activated.')
+
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
