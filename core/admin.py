e02c1deb823b88a8ab17406c819b8ed7ea51ad04
thisisjab <heythisisjab@gmail.com>
2023-07-28 17:55:14 +0330

    feat: complete admin page for core user model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,80 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+from . import forms
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = forms.UserChangeForm
+    model = User
+    list_display = [
+        'id',
+        'username',
+        'email',
+        'is_email_activated',
+        'is_active',
+        'is_staff',
+    ]
+    list_filter = [
+        'username',
+        'email',
+    ]
+    search_fields = [
+        'username',
+        'email',
+    ]
+    ordering = [
+        'username',
+        'email',
+    ]
+    fieldsets = [
+        [
+            None,
+            {
+                'fields': [
+                    'email',
+                    'username',
+                    'password',
+                ]
+            },
+        ],
+        [
+            'Permissions',
+            {
+                'fields': [
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ],
+            },
+        ],
+        [
+            'Important dates',
+            {
+                'fields': [
+                    'last_login',
+                    'date_joined',
+                ],
+            },
+        ],
+    ]
+    add_fieldsets = [
+        [
+            None,
+            {
+                'classes': [
+                    'wide',
+                ],
+                'fields': [
+                    'email',
+                    'username',
+                    'password1',
+                    'password2',
+                ],
+            },
+        ],
+    ]
