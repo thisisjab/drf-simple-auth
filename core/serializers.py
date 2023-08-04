80a85becd5a491c3c408f9cfe9e745f6e0ead0ed
thisisjab <heythisisjab@gmail.com>
2023-08-05 02:01:51 +0330

    refactor: remove validate method in core app UserCreateSerializer and user validate_password directly

--- core/serializers.py
+++ core/serializers.py
@@ -30,32 +30,15 @@ class UserCreateSerializer(UserCreateMixin, serializers.ModelSerializer):
     """
 
     # Since password field is not detected by default, some properties are changed
-    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        validators=[validate_password],
+        write_only=True,
+    )
 
     default_error_messages = {'cannot_create_user': 'Unable to create account'}
 
     class Meta:
         model = User
-        fields = [
-            'id',
-            User.USERNAME_FIELD,
-            *User.REQUIRED_FIELDS,
-            'password'
-        ]
+        fields = ['id', User.USERNAME_FIELD, *User.REQUIRED_FIELDS, 'password']
         read_only_fields = ['id']
-
-    def validate(self, attrs):
-        # Since serializer itself does not check password with validators
-        # defined in `AUTH_PASSWORD_VALIDATORS`, it's checked manually here
-        user = User(**attrs)
-        password = attrs.get('password')
-
-        try:
-            validate_password(password, user)
-        except ValidationError as e:
-            serializer_error = serializers.as_serializer_error(e)
-            raise serializers.ValidationError(
-                {'password': serializer_error}
-            )
-
-        return attrs
