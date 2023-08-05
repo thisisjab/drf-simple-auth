2ea7444f065c20713b1dca5bacb6439919c95f15
thisisjab <heythisisjab@gmail.com>
2023-08-05 14:11:30 +0330

    refactor: move get_user_from_token function from core.views to core.utils

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+from .models import User
 
 
 def encode_uid(pk):
@@ -8,3 +11,27 @@ def encode_uid(pk):
 
 def decode_uid(pk):
     return force_str(urlsafe_base64_decode(pk))
+
+
+def get_user_from_token(uid, token):
+    """Get user from uid and check if token is valid for that user
+
+    Args:
+        uid (str): user id which is base64-encoded
+        token (st): token
+
+    Returns:
+        core.models.User: If token is valid for the user, returns user, otherwise None
+    """
+
+    user_pk = decode_uid(uid)
+
+    try:
+        user = User.objects.get(pk=user_pk)
+    except User.DoesNotExist:
+        return None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        return user
+
+    return None
--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.tokens import default_token_generator
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,31 +14,8 @@ class UserCreateView(CreateAPIView):
 
 
 class UserActivateView(APIView):
-    def get_user_from_token(self, uid, token):
-        """Get user from uid and check if token is valid for that user
-
-        Args:
-            uid (str): user id which is base64-encoded
-            token (st): token
-
-        Returns:
-            core.models.User: If token is valid for the user, returns user, otherwise None
-        """
-
-        user_pk = utils.decode_uid(uid)
-
-        try:
-            user = User.objects.get(pk=user_pk)
-        except User.DoesNotExist:
-            return None
-
-        if user is not None and default_token_generator.check_token(user, token):
-            return user
-
-        return None
-
     def get(self, request, uid, token):
-        user = self.get_user_from_token(uid, token)
+        user = utils.get_user_from_token(uid, token)
         if user:
             user.is_email_activated = True
             user.save()
