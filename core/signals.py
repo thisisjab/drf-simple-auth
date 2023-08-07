c974700bd5ff775e3d8a9ff7124c61ab41817d94
thisisjab <heythisisjab@gmail.com>
2023-08-07 16:22:51 +0330

    feat: log sent email in user post_save signal

--- core/signals.py
+++ core/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import User
+from .models import User, EmailLog
 from .tasks import send_activation_email
 
 
@@ -13,3 +13,5 @@ def user_post_save_send_activation_email(sender, instance, created, **kwargs):
     """
     if created:
         send_activation_email.delay(instance.pk, instance.email)
+        email_log = EmailLog(user=instance, email_type=EmailLog.EMAIL_VERIFICATION)
+        email_log.save()
