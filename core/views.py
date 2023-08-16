b9181e6269251aa39e058e6e9c7cae109a11f51b
= <heythisisjab@gmail.com>
2023-08-17 00:52:27 +0330

    refactor(core): change user CreateView to ListCreateView

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,7 +11,7 @@ from .serializers import UserCreateSerializer
 from . import utils
 
 
-class UserCreateView(CreateAPIView):
+class UserListView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
 
