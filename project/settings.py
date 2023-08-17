f5d1baddc4f8bd6458cd8a207e04050d4e3fc52f
= <heythisisjab@gmail.com>
2023-08-17 14:16:51 +0330

    feat(project): add simplejwt token lifetime settings to project settings file using enviornmnet variables

--- project/settings.py
+++ project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import environ
 import os
@@ -137,6 +138,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+# SimpleJWT configuration
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=env.int('SIMPLE_JWT_ACCESS_TOKEN_LIFETIME')),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=env.int('SIMPLE_JWT_REFRESH_TOKEN_LIFETIME')),
 }
 
 # Internal IPs is used for django debug toolbar
