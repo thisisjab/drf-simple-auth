fe095bcc060d03ae8c43be412098b4c20b4a7d96
= <heythisisjab@gmail.com>
2023-08-18 23:49:50 +0330

    style(core): clean code and sort imports in views

--- core/views.py
+++ core/views.py
@@ -1,13 +1,13 @@
 from django.utils import timezone
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 
-from .models import User, EmailLog
 from .emails import ActivationEmail
+from .models import User, EmailLog
 from .pagination import UserDefaultPagination
 from .serializers import (
     UserCreateSerializer,
@@ -21,7 +21,7 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     pagination_class = UserDefaultPagination
     lookup_field = 'username'
-    
+
     def get_serializer_class(self):
         return (
             UserCreateSerializer
