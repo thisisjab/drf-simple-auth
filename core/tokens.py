75bdd91affd8217dcd52a1bf7468f493b0e05186
= <heythisisjab@gmail.com>
2023-08-19 00:53:42 +0330

    feat(core): create a one time password reset token generator

--- core/tokens.py
+++ core/tokens.py
@@ -13,3 +13,28 @@ class EmailVerificationTokenGenerator(PasswordResetTokenGenerator):
 
 
 email_verification_token_generator = EmailVerificationTokenGenerator()
+
+
+class OneTimePasswordResetTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return str(user.pk) + str(timestamp) + str(user.is_active) + user.password
+
+    def make_token(self, user):
+        token = super().make_token(user)
+        # Store the token in the user's model or a separate model/database
+        user.password_reset_token = token
+        user.save()
+        return token
+
+    def check_token(self, user, token):
+        # Retrieve the stored token from the user's model or database
+        stored_token = user.password_reset_token
+        if token == stored_token:
+            # Reset the stored token after it has been used
+            user.password_reset_token = None
+            user.save()
+            return True
+        return False
+
+
+one_time_token_generator = OneTimePasswordResetTokenGenerator()
