69e2a1d9adc417d4a5ea3d002fe0cc27e06a6d5f
= <heythisisjab@gmail.com>
2023-08-19 20:29:46 +0330

    feat(core): add PasswordResetEmail

--- core/emails.py
+++ core/emails.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from templated_mail.mail import BaseEmailMessage
 
 from .models import User
-from .tokens import email_verification_token_generator
+from .tokens import email_verification_token_generator, one_time_token_generator
 from . import utils
 
 
@@ -30,4 +30,55 @@ class ActivationEmail(BaseEmailMessage):
                 'token': email_verification_token_generator.make_token(user),
             },
         )
+        return context
+
+
+class ActivationEmail(BaseEmailMessage):
+    """
+    This class is used to send an activation email to given user_pk. user_pk
+    should be supplied with context object.
+    """
+    template_name = 'email/activation.html'
+
+    def get_context_data(self):
+        """Generate a token and encode it and return activation_url in context
+
+        Returns:
+            dict: context object
+        """
+        context = super().get_context_data()
+        user_pk = context['user_pk']
+        user = User.objects.get(pk=user_pk)
+        context['username'] = user.username
+        context['activation_url'] = reverse(
+            'user-activate',
+            kwargs={
+                'uid': utils.encode_uid(user.pk),
+                'token': email_verification_token_generator.make_token(user),
+            },
+        )
+        return context
+    
+
+class PasswordResetEmail(BaseEmailMessage):
+    """
+    This class is used to send a password reset email to given user_pk. user_pk
+    should be supplied with context object.
+    """
+    template_name = 'email/password_reset.html'
+
+    def get_context_data(self):
+        """Generate a token and encode it and return in context
+
+        Token is not created here, since first we need it in PasswordResetView.
+
+        Returns:
+            dict: context object
+        """
+        context = super().get_context_data()
+        user_pk = context['user_pk']
+        user = User.objects.get(pk=user_pk)
+        context['username'] = user.username
+        context['uid'] = utils.encode_uid(user.pk)
+
         return context
